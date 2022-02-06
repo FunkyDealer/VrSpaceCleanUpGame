71e6a4d287b5a488073f5cba805775c010ada53e
Funky <59063781+FunkyDealer@users.noreply.github.com>
2022-02-06 19:51:01 +0000

    added new skybox

---
+++ Proj-SpaceCleanUp/Assets/Scripts/Skybox/CameraOrbiter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraOrbiter : MonoBehaviour
+{
+    [SerializeField]
+    GameObject planet;
+
+    [SerializeField]
+    float speed = 0.1f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void FixedUpdate()
+    {
+        transform.RotateAround(planet.transform.position, planet.transform.up, speed);
+    }
+}
---
+++ Proj-SpaceCleanUp/Assets/Scripts/Skybox/SunRotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotator : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 0.1f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(transform.up, speed);
+    }
+
+}
